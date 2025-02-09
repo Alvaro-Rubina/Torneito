14cbac96e376a6c1826e1f97d7714ac33ff6c7ef
Alvaro Rubina <alvarorubina132@gmail.com>
2025-02-09 19:05:34 -0300

    Primer commit

---
+++ src/test/java/org/alvarub/fulbitoapi/FulbitoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alvarub.fulbitoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FulbitoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
