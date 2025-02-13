ec5566214bd1ba89528d2bd9e1563b9dbf2da6b6
Alvaro Rubina <alvarorubina132@gmail.com>
2025-02-13 13:39:14 -0300

    Se creó un ExceptionHandler

---
+++ src/main/java/org/alvarub/fulbitoapi/utils/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package org.alvarub.fulbitoapi.utils.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFoundException(NotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        String errors = ex.getBindingResult().getAllErrors().stream()
+                .map(error -> error.getDefaultMessage())
+                .collect(Collectors.joining(". ")) + ".";
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ src/main/java/org/alvarub/fulbitoapi/utils/exception/NotFoundException.java
@@ -0,0 +1,9 @@
+package org.alvarub.fulbitoapi.utils.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+    super(message);
+  }
+
+}
