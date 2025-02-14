42ce119c9faaccbc109f55bd491a4b97712a9584
Alvaro Rubina <alvarorubina132@gmail.com>
2025-02-13 22:53:55 -0300

    Añadido método de busqueda en SeasonRepository

--- src/main/java/org/alvarub/fulbitoapi/repository/SeasonRepository.java
+++ src/main/java/org/alvarub/fulbitoapi/repository/SeasonRepository.java
@@ -3,5 +3,9 @@ package org.alvarub.fulbitoapi.repository;
 import org.alvarub.fulbitoapi.model.entity.Season;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SeasonRepository extends JpaRepository<Season, Long> {
+
+    Optional<Season> findByCode(String code);
 }
