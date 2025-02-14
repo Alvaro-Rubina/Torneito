75b9ae1c91f8afcbbf536653fdc9953b243343bb
Alvaro Rubina <alvarorubina132@gmail.com>
2025-02-14 01:13:05 -0300

    Añadido metodo al CountryRepository
    Metodo para buscar un pais por su nombre

--- src/main/java/org/alvarub/fulbitoapi/repository/CountryRepository.java
+++ src/main/java/org/alvarub/fulbitoapi/repository/CountryRepository.java
@@ -3,5 +3,9 @@ package org.alvarub.fulbitoapi.repository;
 import org.alvarub.fulbitoapi.model.entity.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CountryRepository extends JpaRepository<Country, Long> {
+
+    Optional<Country> findByName(String name);
 }
