9dadd6c2fbd92388ced0e6c905adbf781b6c7606
Alvaro Rubina <alvarorubina132@gmail.com>
2025-02-13 23:30:29 -0300

    Añadido metodo al LeagueRepository
    Metodo para buscar liga por su nombre

--- src/main/java/org/alvarub/fulbitoapi/repository/LeagueRepository.java
+++ src/main/java/org/alvarub/fulbitoapi/repository/LeagueRepository.java
@@ -3,5 +3,9 @@ package org.alvarub.fulbitoapi.repository;
 import org.alvarub.fulbitoapi.model.entity.League;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LeagueRepository extends JpaRepository<League, Long> {
+
+    Optional<League> findByName(String name);
 }
