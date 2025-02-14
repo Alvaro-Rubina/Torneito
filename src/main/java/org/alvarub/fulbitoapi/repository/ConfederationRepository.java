f9a516b0f0dc4a22638b0ab97300855edcfdab5f
Alvaro Rubina <alvarorubina132@gmail.com>
2025-02-14 12:37:03 -0300

    Añadido metodo al ConfederationRepository
    Metodo para buscar una confederacion por su nombre

--- src/main/java/org/alvarub/fulbitoapi/repository/ConfederationRepository.java
+++ src/main/java/org/alvarub/fulbitoapi/repository/ConfederationRepository.java
@@ -5,6 +5,10 @@ import org.alvarub.fulbitoapi.model.entity.Confederation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ConfederationRepository extends JpaRepository<Confederation, Long> {
+
+    Optional<Confederation> findByName(String name);
 }
