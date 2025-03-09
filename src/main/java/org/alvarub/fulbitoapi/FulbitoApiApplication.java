88ec835bc89f9b2782d55e370ed454e696bdc0ca
Alvaro Rubina <alvarorubina132@gmail.com>
2025-03-09 02:11:28 -0300

    Habilitado cacheo

--- src/main/java/org/alvarub/fulbitoapi/FulbitoApiApplication.java
+++ src/main/java/org/alvarub/fulbitoapi/FulbitoApiApplication.java
@@ -2,8 +2,10 @@ package org.alvarub.fulbitoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class FulbitoApiApplication {
 
 	public static void main(String[] args) {
