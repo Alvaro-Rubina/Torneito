b51e55bc3d93f4d6d1821b80c363f67e57f6c2fd
Alvaro Rubina <alvarorubina132@gmail.com>
2025-04-15 06:44:32 -0300

    Arreglado LeagueService

--- src/main/java/org/alvarub/fulbitoapi/service/LeagueService.java
+++ src/main/java/org/alvarub/fulbitoapi/service/LeagueService.java
@@ -2,11 +2,9 @@ package org.alvarub.fulbitoapi.service;
 
 import org.alvarub.fulbitoapi.model.dto.LeagueDTO;
 import org.alvarub.fulbitoapi.model.dto.LeagueResponseDTO;
-import org.alvarub.fulbitoapi.model.dto.SeasonResponseDTO;
 import org.alvarub.fulbitoapi.model.entity.League;
-import org.alvarub.fulbitoapi.model.entity.Season;
+import org.alvarub.fulbitoapi.repository.CountryRepository;
 import org.alvarub.fulbitoapi.repository.LeagueRepository;
-import org.alvarub.fulbitoapi.repository.SeasonRepository;
 import org.alvarub.fulbitoapi.utils.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
@@ -24,26 +22,15 @@ public class LeagueService {
     private LeagueRepository leagueRepo;
 
     @Autowired
-    private SeasonService seasonService;
-
+    private CountryRepository countryRepo;
     @Autowired
-    private SeasonRepository seasonRepo;
+    private CountryService countryService;
 
     @CacheEvict(value = "leagues", allEntries = true)
     public void saveLeague(LeagueDTO leagueDTO) {
         League league = toEntity(leagueDTO);
         leagueRepo.save(league);
 
-    }
-
-    @Cacheable("leagues")
-    public List<LeagueResponseDTO> findAllLeagues() {
-        List<League> leagues = leagueRepo.findAll();
-        List<LeagueResponseDTO> leaguesResponse = new ArrayList<>();
-        for (League league: leagues) {
-            leaguesResponse.add(toDto(league));
-        }
-        return leaguesResponse;
     }
 
     @Cacheable(value = "leagues", key = "#id")
@@ -62,61 +49,53 @@ public class LeagueService {
         return toDto(league);
     }
 
+    @Cacheable("leagues")
+    public List<LeagueResponseDTO> findAllLeagues() {
+        List<League> leagues = leagueRepo.findAll();
+        List<LeagueResponseDTO> leaguesDTO = new ArrayList<>();
+        for (League league: leagues) {
+            leaguesDTO.add(toDto(league));
+        }
+        return leaguesDTO;
+    }
+
     @CachePut(value = "leagues", key = "#id")
     public void editLeague(Long id, LeagueDTO leagueDTO) {
-        League existingLeague = leagueRepo.findById(id)
+        League league = leagueRepo.findById(id)
                 .orElseThrow(() -> new NotFoundException("Liga con el id '" + id + "' no encontrada"));
 
-        if (leagueDTO.getName() != null) {
-            existingLeague.setName(leagueDTO.getName());
-        }
-        if (leagueDTO.getLogo() != null) {
-            existingLeague.setLogo(leagueDTO.getLogo());
-        }
-        if (leagueDTO.getCountrieName() != null) {
-            existingLeague.setCountrieName(leagueDTO.getCountrieName());
-        }
-        if (leagueDTO.getSeasons() != null && !leagueDTO.getSeasons().isEmpty()) {
-            List<Season> seasons = new ArrayList<>();
-            for (String seasonCode : leagueDTO.getSeasons()) {
-                seasons.add(seasonRepo.findByCode(seasonCode)
-                        .orElseThrow(() -> new NotFoundException("Temporada con código '" + seasonCode + "' no encontrada")));
-            }
-            existingLeague.setSeasons(seasons);
+        if (!league.getName().equals(leagueDTO.getName())) {
+            league.setName(leagueDTO.getName());
         }
 
-        leagueRepo.save(existingLeague);
+        if (!league.getLogo().equals(leagueDTO.getLogo())) {
+            league.setLogo(leagueDTO.getLogo());
+        }
+
+        if (!league.getCountry().getId().equals(leagueDTO.getCountryId())) {
+            league.setCountry(countryRepo.findById(leagueDTO.getCountryId())
+                    .orElseThrow(() -> new NotFoundException("País con el id '" + leagueDTO.getCountryId() + "' no encontrado")));
+        }
+
+        leagueRepo.save(league);
     }
 
     // Mappers
     private League toEntity(LeagueDTO leagueDTO) {
-        List<Season> seasons = new ArrayList<>();
-
-        for (String season: leagueDTO.getSeasons()) {
-            seasons.add(seasonRepo.findByCode(season)
-                    .orElseThrow(() -> new NotFoundException("Temporada con código '" + season + "' no encontrada")));
-        }
-
         return League.builder()
                 .name(leagueDTO.getName())
                 .logo(leagueDTO.getLogo())
-                .countrieName(leagueDTO.getCountrieName())
-                .seasons(seasons)
+                .country(countryRepo.findById(leagueDTO.getCountryId())
+                        .orElseThrow(() -> new NotFoundException("País con el id '" + leagueDTO.getCountryId() + "' no encontrado")))
                 .build();
     }
 
     public LeagueResponseDTO toDto(League league) {
-        List<SeasonResponseDTO> seasonsResponse = new ArrayList<>();
-        for (Season season: league.getSeasons()) {
-            seasonsResponse.add(seasonService.toDto(season));
-        }
-
         return LeagueResponseDTO.builder()
                 .id(league.getId())
                 .name(league.getName())
                 .logo(league.getLogo())
-                .countrieName(league.getCountrieName())
-                .seasons(seasonsResponse)
+                .country(countryService.toDto(league.getCountry()))
                 .build();
     }
 }
